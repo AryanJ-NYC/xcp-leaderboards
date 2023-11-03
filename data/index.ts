1af821b59c1af68fc982c56b21f837bfc5531b07
Aryan Jabbari <AryanJabbari@gmail.com>
2023-11-03 19:37:28 -0400

    fix(prisma): update exporter

--- data/index.ts
+++ data/index.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-// add prisma to the NodeJS global type
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-// Prevent multiple instances of Prisma Client in development
-declare const global: CustomNodeJsGlobal;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const prisma = global.prisma ?? new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export * from '@prisma/client';
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
