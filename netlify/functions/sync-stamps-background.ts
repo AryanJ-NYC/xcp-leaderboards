506345f2270cfe001420c0f4bf89df159f64c3b5
Aryan Jabbari <AryanJabbari@gmail.com>
2023-11-03 19:16:17 -0400

    feat(stamps): remove

--- netlify/functions/sync-stamps-background.ts
+++
@@ -1,7 +0,0 @@
-import type { BackgroundHandler } from '@netlify/functions';
-import { getStamps } from '../../packages/projects';
-import { sync } from '../sync';
-
-export const handler: BackgroundHandler = async () => {
-  await sync(getStamps, 'stamps');
-};
