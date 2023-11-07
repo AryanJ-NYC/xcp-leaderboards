6ac89e8de2818922f0402a2251d8b8c95d80f5ad
Aryan Jabbari <AryanJabbari@gmail.com>
2023-11-07 11:25:43 -0500

    Upgrade counterparty-node-client

--- web-app/app/api/sync-apes/route.ts
+++ web-app/app/api/sync-apes/route.ts
@@ -6,4 +6,9 @@ export const POST = async () => {
   await sync(getDabcList, 'drooling-apes');
   return new NextResponse();
 };
+
+export const unstable_allowDynamic = [
+  // use a glob to allow anything in the function-bind 3rd party module
+  '/node_modules/counterparty-node-client/**',
+];
 export const runtime = 'edge';
